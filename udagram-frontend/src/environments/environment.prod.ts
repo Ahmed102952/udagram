b6d569ffc7d05714fb07a08a5884cf1d83da3407
Ahmed102952 <ahmeda8400@gmail.com>
2022-06-30 19:58:06 +0200

    .

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api.eba-iki96nsd.us-east-1.elasticbeanstalk.com",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api.eba-iki96nsd.us-east-1.elasticbeanstalk.com',
 };
 
 /*
